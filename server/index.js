7bd1886674921345f3fb1b0917acbc6a059a5039
ChealseaYao <successsssssss1126@outlook.com>
2024-12-02 22:39:14 +1030

    first day

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRouters from './routes/posts.js'
+
+// const express = require('express');
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use('/', postRouters)
+
+const CONNECTION_URL = 'mongodb+srv://mernmemories:mernmemories123@cluster0.e5agvuw.mongodb.net/';
+
+const PORT = process.env.PORT || 5555;
+
+mongoose.connect(CONNECTION_URL)
+.then(()=>app.listen(PORT, ()=>console.log(`server running on port ${PORT}`)))
+.catch(((error)=>console.log(error.message)));
+
