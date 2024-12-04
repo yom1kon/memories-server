2256a37cb7cba84fab46376de47b28c7f760dc37
ChealseaYao <successsssssss1126@outlook.com>
2024-12-04 14:06:57 +1030

    fix

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/', postRouters)
-app.get('./',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send('hello to memories api')
 })
 // const CONNECTION_URL = 'mongodb+srv://mernmemories:mernmemories123@cluster0.e5agvuw.mongodb.net/';
