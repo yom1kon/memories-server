7aee511d9d6fbdbcb21623eb90b1b6dc8993d4af
ChealseaYao <successsssssss1126@outlook.com>
2024-12-07 14:20:28 +1030

    fix url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import userRouters from './routes/user.js'
 
 // const express = require('express');
 const app = express();
-// dotenv.config();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -21,11 +21,11 @@ app.use('/user',userRouters);
 app.get('/',(req,res)=>{
     res.send('hello to memories api')
 })
-const CONNECTION_URL = 'mongodb+srv://mernmemories:mernmemories123@cluster0.e5agvuw.mongodb.net/';
+// const CONNECTION_URL = 'mongodb+srv://mernmemories:mernmemories123@cluster0.e5agvuw.mongodb.net/';
 
 const PORT = process.env.PORT || 5555;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
 .then(()=>app.listen(PORT, ()=>console.log(`server running on port ${PORT}`)))
 .catch(((error)=>console.log(error.message)));
 
